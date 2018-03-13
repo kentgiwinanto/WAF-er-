57c691959da44fdbe5f6d337dd071ed64be6a551
ciangfongyou <32587124+ciangfongyou@users.noreply.github.com>
2018-03-13 17:04:34 +0700

    13/03/2018
    minus register page and config

--- js/login.js
+++ js/login.js
@@ -1,12 +1,13 @@
 function check(){
 
-	var username= document.getElementById('user').value;
-	var password= document.getElementById('pass').value;
+	var username= document.getElementById('usernametxt').value;
+	var password= document.getElementById('passwordtxt').value;
 
 
 	if(username.length<=4 || username.length>=21){
 		alert("Invalid Username or Password");
-	}else if(!username.match("^[a-zA-Z0-9_]*$")){
+	}
+	else if(!username.match("^[a-zA-Z0-9_]*$")){
 	 	alert("Illegal input detected");
 	}else if(password.length<=4 || password.length>=21){
 		alert("Invalid Username or Password");
