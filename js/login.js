87b49d21b7b9c7acd16542221e960576a3f38c19
ciangfongyou <32587124+ciangfongyou@users.noreply.github.com>
2018-03-15 14:52:31 +0700

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -2,23 +2,24 @@ function check(){
 
 	var username= document.getElementById('usernametxt').value;
 	var password= document.getElementById('passwordtxt').value;
+	var errormsg = document.getElementById('errortxt');
 
 
 	if(username.length<=4 || username.length>=21){
-		alert("Invalid Username or Password");
+		 document.getElementById("errortxt").innerHTML="Invalid Username and Password";
 	}
 	else if(!username.match("^[a-zA-Z0-9_]*$")){
-	 	alert("Illegal input detected");
+	 	document.getElementById("errortxt").innerHTML="Invalid Username and Password";
 	}
 	else if (username.match(/[<>"'/&-:;.,"]/)){
-		alert("Illegal input detected");
+		document.getElementById("errortxt").innerHTML="Invalid Username and Password";
 	}
 	else if(password.length<=4 || password.length>=21){
-		alert("Invalid Username or Password");
+		document.getElementById("errortxt").innerHTML="Invalid Username and Password";
 	}else if(password.match(/[<>"'/&-:;.,]/)){
-		alert("Illegal input detected");
+		document.getElementById("errortxt").innerHTML="Invalid Username and Password";
 	}else{
-		alert("success")
+		document.getElementById("errortxt").innerHTML=""
 		open ("home.html");
 	}
 
--- js/register.js
+++ js/register.js
@@ -6,28 +6,30 @@ function checkregis() {
 	var jobpost = document.getElementById("jobpostcheck").value;
 	var adminpost = document.getElementById("admincheck").checked;
 	var staffpost = document.getElementById("staffcheck").checked;
+	var errormsg = document.getElementById("errortxt").innerHTML;
+
 if (firstname =="" || lastname =="") {
-	alert("Please input Your Name ! ");
+	document.getElementById("errortxt").innerHTML="Invalid Name";
 } else if (!firstname.match("^[a-zA-Z_]*$")){
-	alert("Name Must Alphabet");
+	document.getElementById("errortxt").innerHTML="Invalid Name";
 } else if (!lastname.match("^[a-zA-Z_]*$")){
-	alert("Name Must Alphabet");
+	document.getElementById("errortxt").innerHTML="Invalid Name";
 }
 else if (firstname.length>=32 || lastname.length>=32)
 {
-alert("First Name and Last Name must be each 32 Characters");
+document.getElementById("errortxt").innerHTML="Invalid Name";
 } else if (password =="") {
-	alert("Password must Be Filled");
+	document.getElementById("errortxt").innerHTML="Invalid Password";
 } else if (password.match(/[<>"'/&-:;.,]/)){
-	alert("Ilegal Input on Password");
+	document.getElementById("errortxt").innerHTML="Invalid Password";
 }
 else if (confirm!=password){
-	alert("Confirm Password Does not Match");
+	document.getElementById("errortxt").innerHTML="Confirm Password Does not Match";
 }
  else if (jobpost==""){
-	alert("Please Choose the Job Post !");
+	document.getElementById("errortxt").innerHTML="Choose the job Position";
 }
 else 
-	{ alert("Success");}
+	{ document.getElementById("errortxt").innerHTML="";}
 }
 
