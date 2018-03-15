5ac45922273bf1ffa06d30575e604f8d1f9dd596
zarakhiel <rizkyg@gmail.com>
2018-03-15 17:30:38 +0700

    DATABASE UPDATED with encryption table

--- Database/WAFer.sql
+++ Database/WAFer.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE WAFer
 
-USE WAFer
+
 CREATE TABLE UserLogin(
 	STSRC CHAR(1),
     DateIN DATETIME,
@@ -12,8 +12,9 @@ CREATE TABLE UserLogin(
     UserLoginID VARCHAR(767) PRIMARY KEY,
     Username VARCHAR(32) NOT NULL,
     Userpass VARCHAR(32) NOT NULL
-    )
+    );
     
+
 
 
 CREATE TABLE UserProfile(
@@ -28,7 +29,7 @@ CREATE TABLE UserProfile(
     UserUP VARCHAR(32),
     UserJobPosID VARCHAR(767) NOT NULL,
     FOREIGN KEY (UserJobPosID) REFERENCES UserJobPosition(UserJobPosID) ON UPDATE CASCADE ON DELETE CASCADE
-)
+);
 
 
 
@@ -40,11 +41,34 @@ CREATE TABLE UserJobPosition(
     UserIN VARCHAR(32),
     DateUP DATETIME,
     UserUP VARCHAR(32) 
+    );
+    
+    
+
+CREATE TABLE SavedEncryptedLog (
+	STSRC VARCHAR(1),
+    DateIN DATETIME,
+    UserIN VARCHAR(32),
+    DateUP DATETIME,
+    UserUP VARCHAR(32),
+    logID VARCHAR(767),
+    Daytime DATETIME,
+    ServerIP VARCHAR(50),
+    MessageEncrypted TEXT,
+    EncryptionType VARCHAR(1000),
+    EncryptionKey TEXT
+    );
+    
+    CREATE TABLE MsEncryptionType (
+    STSRC  VARCHAR(1),
+    DateIN DATETIME,
+    UserIN VARCHAR(32),
+    DateUP DATETIME,
+    UserUP VARCHAR(32),
+    EncryptionTypeID VARCHAR(1000),
+    EncryptionTypeName VARCHAR(1000)
     )
     
-    use WAFer
-    
-CREATE TABLE Transactions(
 	TransactionID VARCHAR(767) PRIMARY KEY,
     clientIP VARCHAR(50),
     daytime VARCHAR(767),
@@ -55,8 +79,7 @@ CREATE TABLE Transactions(
 	requestID VARCHAR(767),
 	responseID VARCHAR(767),
 	producerID VARCHAR(767),
-	messageID VARCHAR (767),
-    STSRC VARCHAR(1),
+	STSRC VARCHAR(1),
     DateIN DATETIME,
     UserIN VARCHAR(32),
     DateUP DATETIME,
@@ -64,136 +87,10 @@ CREATE TABLE Transactions(
     FOREIGN KEY (requestID) REFERENCES Requests(requestID) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (responseID) REFERENCES Responses(responseID) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (producerID) REFERENCES Producer(producerID) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (messageID) REFERENCES Messages(messageID) ON UPDATE CASCADE ON DELETE CASCADE
-    )
+    );
     
-CREATE TABLE ReqHeaders(
-	reqheader VARCHAR(767) PRIMARY KEY,
-	host VARCHAR(767),
-	useragent VARCHAR(767),
-	cookie VARCHAR(767),
-	accept VARCHAR(767),
-	acceptlang VARCHAR(767),
-	acceptencd VARCHAR(767),
-	connection VARCHAR(767),
-	pragma VARCHAR(767),
-	cachecontrol VARCHAR(767),
-	upgrade_insecure_request VARCHAR(767),
-	STSRC VARCHAR(1),
-    UserIN VARCHAR(32),
-	DateIN DATETIME,
-    UserUP VARCHAR(32),
-    DateUP DATETIME
-    )
-    
-CREATE TABLE Requests (
-    requestID VARCHAR(767) Primary Key,
-    method VARCHAR(767),
-    httpversion VARCHAR(767),
-    URI VARCHAR(767),
-    reqheader VARCHAR(767),
-    FOREIGN KEY (reqheader) REFERENCES ReqHeaders(reqheader) ON UPDATE CASCADE ON DELETE CASCADE,
-    STSRC VARCHAR(1),
-    UserIN VARCHAR(32),
-	DateIN DATETIME,
-    UserUP VARCHAR(32),
-    DateUP DATETIME
-	)
-    
-CREATE TABLE ResHeaders (
-	resheader VARCHAR(767)PRIMARY KEY,
-    server VARCHAR(100),
-	date VARCHAR(100),
-	contentleng VARCHAR(767),
-	contenttype VARCHAR(767),
-	connection VARCHAR(767),
-    STSRC VARCHAR(1),
-    UserIN VARCHAR(32),
-	DateIN DATETIME,
-    UserUP VARCHAR(32),
-    DateUP DATETIME
-    )
-    
-CREATE TABLE Responses (
-	responseID VARCHAR(767) PRIMARY KEY,
-    httpcode VARCHAR(767),
-    resheader VARCHAR(767),
-    FOREIGN KEY (resheader) REFERENCES ResHeaders(resheader) ON UPDATE CASCADE ON DELETE CASCADE,
-    STSRC VARCHAR(1),
-    UserIN VARCHAR(32),
-	DateIN DATETIME,
-    UserUP VARCHAR(32),
-    DateUP DATETIME
-    )
-    
-CREATE TABLE Producer (
-	producerID VARCHAR(767) PRIMARY KEY,
-	modsec VARCHAR(767),
-	connector VARCHAR(767),
-	secrule_eng VARCHAR(767),
-	components VARCHAR(767),
-	STSRC VARCHAR(1),
-    UserIN VARCHAR(32),
-	DateIN DATETIME,
-    UserUP VARCHAR(32),
-    DateUP DATETIME
-	)
-    
-CREATE TABLE Messages (
-	messageID VARCHAR(767)PRIMARY KEY,
-	themessages VARCHAR(767),
-	msgmatch VARCHAR(767),
-    reqheader VARCHAR(767),
-	FOREIGN KEY (reqheader) REFERENCES ReqHeaders(reqheader) ON UPDATE CASCADE ON DELETE CASCADE,
-	severity VARCHAR(3),
-	ver VARCHAR(100),
-	rev VARCHAR(100),
-	maturity VARCHAR(3),
-	accuracy VARCHAR(3),
-    STSRC VARCHAR(1),
-    UserIN VARCHAR(32),
-	DateIN DATETIME,
-    UserUP VARCHAR(32),
-    DateUP DATETIME
-	)
-    
-CREATE TABLE DetailMessageTag(
-	TagID VARCHAR(100),
-	messageID VARCHAR(767),
-	FOREIGN KEY (messageID) REFERENCES Messages(MessageID) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (TagID) REFERENCES Tag(TagID) ON UPDATE CASCADE ON DELETE CASCADE,
-	STSRC VARCHAR(1),
-    UserIN VARCHAR(32),
-	DateIN DATETIME,
-    UserUP VARCHAR(32),
-    DateUP DATETIME
-    )
-    
-    DROP TABLE DetailMessageTag
-    
-CREATE TABLE Tag(
-	TagID VARCHAR(100) PRIMARY KEY,
-    Tagname VARCHAR(200),
-    STSRC VARCHAR(1),
-    UserIN VARCHAR(32),
-	DateIN DATETIME,
-    UserUP VARCHAR(32),
-    DateUP DATETIME
-	)
-    
-CREATE TABLE TransactionDetails(
-    TransactionID VARCHAR(767),
-    messageID VARCHAR(767),
-    FOREIGN KEY (TransactionID) REFERENCES Transactions(TransactionID) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (messageID) REFERENCES Messages(messageID) ON UPDATE CASCADE ON DELETE CASCADE,
-    STSRC VARCHAR(1),
-    UserIN VARCHAR(32),
-	DateIN DATETIME,
-    UserUP VARCHAR(32),
-    DateUP DATETIME
-    )
-
-    DROP TABLE TransactionDetails
+   
+  
     
     DELIMITER $$
 	CREATE PROCEDURE WAF_Insert_AdminUser ()
@@ -233,7 +130,7 @@ CREATE TABLE TransactionDetails(
 	END IF;
     END;
     
-    DROP PROCEDURE WAF_Insert_Register
+
     
      
 
@@ -310,7 +207,9 @@ CREATE TABLE TransactionDetails(
     DELETE FROM UserJobPosition
     WHERE UserJobPosID=posiddel;
     END;
+
     
+      
     DELIMITER $$
     CREATE PROCEDURE WAF_Update_UserLogin (userloginid VARCHAR(767), unameup VARCHAR(32), upassup VARCHAR(32))
     BEGIN
@@ -328,20 +227,4 @@ CREATE TABLE TransactionDetails(
         SET alert="Update Successful";
 	END IF;
     END;
-    
-    DELIMITER $$
-    CREATE PROCEDURE WAF_Insert_Log(
-    
-    
-    
-    
-    
-	
-    
-
-    
- 
-
-    
-    
     
