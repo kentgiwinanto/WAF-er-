f6f53b7b1e6c43d4b42feeb018e781c01dbb0883
ciangfongyou <jffcomm@gmail.com>
2018-04-02 17:01:32 +0700

    FrontENd 2 April

---
+++ js/container.js
@@ -0,0 +1,17 @@
+function addcont1 () {
+	$('#container1').clone().appendTo('#container').removeAttr('id').attr('id','container'+nomor).attr('testong',nomor).css('display','');
+	$('#container'+nomor).children('#addsmallcontainer1').removeAttr('id').attr('id','addsmallcontainer'+nomor).attr('nomor',nomor);
+	$("#addsmallcontainer"+nomor).click(function(){
+		var tempnomor = $(this).attr('nomor');
+	    $('#smallcontainer1').clone().appendTo("#container"+tempnomor).removeAttr('id').attr('id','smallcontainer'+tempnomor).css('display','');
+	});
+	nomor = nomor + 1;
+	
+}
+
+
+// function addcont2 () {
+// 	console.log(attr('testong'));
+// 	$('#smallcontainer1').clone().appendTo("#container3").removeAttr('id').attr('id','smallcontainer'+nomor).css('display','')
+// }
+
