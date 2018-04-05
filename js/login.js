d32f910864aaf1aa5db6b87511db6e927535f6d5
ciangfongyou <jffcomm@gmail.com>
2018-04-05 14:30:05 +0700

    ADD 5 APRIL

--- js/login.js
+++ js/login.js
@@ -8,11 +8,8 @@ function check(){
 	if(username.length<=4 || username.length>=21){
 		 document.getElementById("errortxt").innerHTML="Invalid Username and Password";
 	}
-	else if(!username.match("^[a-zA-Z0-9_]*$")){
+	else if(username.match(/[^a-zA-Z\.]/)){
 	 	document.getElementById("errortxt").innerHTML="Invalid Username and Password";
-	}
-	else if (username.match(/[<>"'/&-:;.,"]/)){
-		document.getElementById("errortxt").innerHTML="Invalid Username and Password";
 	}
 	else if(password.length<=4 || password.length>=21){
 		document.getElementById("errortxt").innerHTML="Invalid Username and Password";
