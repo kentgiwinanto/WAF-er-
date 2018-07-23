010f79c37eaa8d35a36e1171e92a4f51df197596
kentgiwinanto <kentgiwinanto@gmail.com>
2018-07-23 13:10:56 +0700

    Add Server.html and login.html to flask

--- App.py
+++ App.py
@@ -6,8 +6,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+	return render_template('index.html')
+
+@app.route('/server')
+def server():
+	return render_template('server.html')
+
+@app.route('/login')
+def login():
+	return render_template('login.html')
 
 if __name__ == "__main__":
-    app.secret_key = os.urandom(12)
-    app.run(debug=True)
+	app.secret_key = os.urandom(12)
+	app.run(debug=True)
