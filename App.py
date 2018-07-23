d5d106a5e9eea09207897bd9a468f8738523c166
kentgiwinanto <kentgiwinanto@gmail.com>
2018-07-23 14:52:23 +0700

    Add Login function

--- App.py
+++ App.py
@@ -1,21 +1,79 @@
-from flask import Flask
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, request, session, redirect, url_for, abort, render_template, flash, jsonify
+from flaskext.mysql import MySQL
+from functools import wraps
 import os
 
-app = Flask(__name__)
+_app = Flask(__name__)
+mysql = MySQL()
 
-@app.route('/')
+_app.config.update(dict(
+	SECRET_KEY = 'h[#T@t$bKvya*h[#T@t$bKvya*h[#T@t$bKvya*ctr(^ctr(^ctr(^',
+))
+
+#MySQL Configuration
+_Database_Username = 'root'
+_Database_Password = 'toor'
+_Database_Database = 'WAFer'
+_Database_Host = '127.0.0.1'
+
+_app.config['MYSQL_DATABASE_USER'] = _Database_Username
+_app.config['MYSQL_DATABASE_PASSWORD'] = _Database_Password
+_app.config['MYSQL_DATABASE_DB'] = _Database_Database
+_app.config['MYSQL_DATABASE_HOST'] = _Database_Host
+mysql.init_app(_app)
+
+#Check is session exists
+def is_logged_in(f):
+	@wraps(f)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return f(*args, **kwargs)
+		else:
+			return render_template('login.html',errorMessage="You must login first!")
+	return wrap
+
+@_app.route('/')
+@is_logged_in
 def home():
 	return render_template('index.html')
 
-@app.route('/server')
+@_app.route('/server')
+@is_logged_in
 def server():
 	return render_template('server.html')
 
-@app.route('/login')
+@_app.route('/login',methods=['POST'])
 def login():
-	return render_template('login.html')
+	_Username_Login_Form = request.form['username']
+	_Password_Login_Form = request.form['password']
+	# try:
+	error = None
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	args = [_Username_Login_Form,_Password_Login_Form]
+	cursor.callproc('WAF_Read_Login',args)
+	conn.commit()
+	Result = cursor.fetchall()
+	if len(Result) != 0:
+		session['logged_in'] = True
+		session["userProfileID"] = Result[0][1]
+		session["userProfileName"] = Result[0][0]
+		session["UserJobPosID"] = Result[0][2]
+		session["UserJobPosName"] = Result[0][3]
+		return redirect(url_for('home'))
+	else:
+		error = "Invalid Username or Password!"
+		return render_template('login.html',errorMessage=error)
+
+@_app.route('/logout')
+def logout():
+	session.pop("logged_in")
+	session.pop("userProfileID")
+	session.pop("userProfileName")
+	session.pop("UserJobPosID")
+	session.pop("UserJobPosName")
+	return redirect(url_for('home'))
 
 if __name__ == "__main__":
-	app.secret_key = os.urandom(12)
-	app.run(debug=True)
+	_app.secret_key = os.urandom(12)
+	_app.run(debug=True)
