30e752fd0cf576bc47e652956b8fba8fc84bb1ab
zarakhiel <rizkyg@gmail.com>
2018-07-25 16:17:46 +0700

    ADD CHANGE PASSWORD PAGE, AND FIX ERROR AT ICON SERVER

--- js/MainPageChart.js
+++ js/MainPageChart.js
@@ -6,9 +6,9 @@ var chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
         datasets: [{
-            label: "My First dataset",
+            label: "Access Daily",
             backgroundColor: '#97ce2f',
             borderColor: '#97ce2f',
             data: [0, 10, 5, 2, 20, 30, 45],
@@ -27,12 +27,12 @@ var chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        labels: ["June", "July", "Augustus", "September"],
         datasets: [{
-            label: "My First dataset",
+            label: "Total Attack",
             backgroundColor: '#97ce2f',
             borderColor: '#97ce2f',
-            data: [0, 10, 5, 2, 20, 30, 45],
+            data: [24, 26, 30, 40],
         }]
     },
 
@@ -44,11 +44,74 @@ var chart = new Chart(ctx, {
 var ctx = document.getElementById('myChart3').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
+    type: 'horizontalBar',
+
+    // The data for our dataset
+    data: {
+        labels: [ "GET", "POST", "PUT"],
+        datasets: [{
+            label: "Method",
+            backgroundColor: '#97ce2f',
+            borderColor: '#97ce2f',
+            data: [30, 10, 5],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+//4
+var ctx = document.getElementById('myChart4').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
     type: 'doughnut',
 
     // The data for our dataset
     data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        labels: [ "200", "404", "502"],
+        datasets: [{
+            label: "Status Code",
+            backgroundColor: '#97ce2f',
+            borderColor: '#97ce2f',
+            data: [10, 10, 5],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+//5
+var ctx = document.getElementById('myChart5').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'radar',
+
+    // The data for our dataset
+    data: {
+        labels: [ "Chrome", "FireFox", "Safari", "Komodo","IE"],
+        datasets: [{
+            label: "User Agent",
+            backgroundColor: '#97ce2f',
+            borderColor: '#97ce2f',
+            data: [14, 10, 5, 7, 8, 7],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+//6
+var ctx = document.getElementById('myChart6').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'pie',
+
+    // The data for our dataset
+    data: {
+        labels: [ "May", "June", "July"],
         datasets: [{
             label: "My First dataset",
             backgroundColor: '#97ce2f',
