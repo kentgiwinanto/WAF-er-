39ae76f230d23e1590b0c8f6cc7b8b9a849744a1
kentgiwinanto <kentgiwinanto@gmail.com>
2018-07-27 15:33:48 +0700

    Checkpoint #1 (Fix Server page and implement popup detail log index)

--- App.py
+++ App.py
@@ -22,14 +22,18 @@ __app.config['MYSQL_DATABASE_DB'] = __Database_Database
 __app.config['MYSQL_DATABASE_HOST'] = __Database_Host
 mysql.init_app(__app)
 
-#Variables
+###Variables
 __ReverseProxy_Address = "http://192.168.56.103:9112/"
+__headers_requests = {
+    'User-Agent': 'WAFer Crawler/1.0'
+}
 
 __conn = mysql.connect()
 __cursor = __conn.cursor()
 
-#Decorators
-#Check is session exists
+
+### Decorators section
+# Check is session exists
 def is_logged_in(f):
 	@wraps(f)
 	def wrap(*args, **kwargs):
@@ -39,28 +43,47 @@ def is_logged_in(f):
 			return render_template('login.html',errorMessage="You must login first!")
 	return wrap
 
-#functions
+### functions section
 def SendRequestAddServer(__ServerName,__IPAddress,__PortAddress,__Hostname,__ModSecurityVal):
-	r = requests.post(__ReverseProxy_Address+"addServer.php",data = {
-			'ServerName':__ServerName,
-			'IP':__IPAddress,
-			'Port':__PortAddress,
-			'Domain':__Hostname,
-			'ModSecurity':__ModSecurityVal
-		})
+	r = requests.post(
+			__ReverseProxy_Address+"addServer.php",
+			headers = __headers_requests,
+			data = {
+				'ServerName':__ServerName,
+				'IP':__IPAddress,
+				'Port':__PortAddress,
+				'Domain':__Hostname,
+				'ModSecurity':__ModSecurityVal
+			}
+		)
 	return r.text
 
 def SendRequestPingServer(__JSONDump):
-	r = requests.post(__ReverseProxy_Address+"pingServer.php", data = {
-			'ListIPJSON':__JSONDump
-		})
+	r = requests.post(
+			__ReverseProxy_Address+"pingServer.php",
+			headers = __headers_requests,
+			data = {
+				'ListIPJSON':__JSONDump
+			}
+		)
 	return r.text
 
 def GetLogsFromServer():
+	# Get Logs from Server and then convert all list into dict
 	global __SecLogs_WAF_ReverseProxy
 	global __AccessLogs_Nginx_ReverseProxy
-	__SecLogs_WAF_ReverseProxy = json.loads(requests.get(__ReverseProxy_Address+"GetLogs.php").text)
-	__AccessLogs_Nginx_ReverseProxy = json.loads(requests.get(__ReverseProxy_Address+"GetAccessLogs.php").text)
+	__SecLogs_WAF_ReverseProxy = json.loads(
+									requests.get(
+										__ReverseProxy_Address+"GetLogs.php",
+										headers = __headers_requests
+									).text
+								)
+	__AccessLogs_Nginx_ReverseProxy = json.loads(
+										requests.get(
+											__ReverseProxy_Address+"GetAccessLogs.php",
+											headers = __headers_requests
+										).text
+									)
 	
 	for key,value in enumerate(__SecLogs_WAF_ReverseProxy):
 		__SecLogs_WAF_ReverseProxy[key] = json.loads(value)
@@ -68,18 +91,13 @@ def GetLogsFromServer():
 	for key,value in enumerate(__AccessLogs_Nginx_ReverseProxy):
 		__AccessLogs_Nginx_ReverseProxy[key] = json.loads(value)
 
-	print(type(__SecLogs_WAF_ReverseProxy[0]))
-
-#routes and business process logic
+### routes and business process logic
 @__app.route('/')
 @is_logged_in
 def home():
 	GetLogsFromServer()
-	return render_template('index.html',SecurityLog=__SecLogs_WAF_ReverseProxy,AccessLogs=__AccessLogs_Nginx_ReverseProxy)
 
-@__app.route('/server')
-@is_logged_in
-def server():
+	#Get Server List
 	__cursor.callproc('WAF_Read_ServerList')
 	__conn.commit()
 	__Result = __cursor.fetchall()
@@ -95,8 +113,10 @@ def server():
 			'ModSecurity':value[5]
 		})
 
-	__FinalResult = json.loads(SendRequestPingServer(json.dumps(__ServerList)))
-	return render_template('server.html',ServerListResult=__FinalResult)
+	global __FinalResult_ServerList
+	__FinalResult_ServerList = json.loads(SendRequestPingServer(json.dumps(__ServerList)))
+
+	return render_template('index.html',SecurityLog=__SecLogs_WAF_ReverseProxy,AccessLogs=__AccessLogs_Nginx_ReverseProxy,ServerListResult=__FinalResult_ServerList)
 
 @__app.route('/addServer',methods=['GET','POST'])
 @is_logged_in
@@ -106,12 +126,15 @@ def addServer():
 		__IPAddress_addServer_Form = request.form['IPAddress']
 		__PortOpen_addServer_Form = request.form['PortsOpen']
 		__Hostname_addServer_Form = request.form['DomainORhostname']
-		__ModSec_addServer_Form = str(request.form.getlist("ModSecurityVal")[0] if None else 0)
-		
+		try:
+			__ModSec_addServer_Form = request.form.getlist("ModSecurityVal")[0]
+		except Exception as e:
+			__ModSec_addServer_Form = '0'
 		__args = [session['userProfileID'],__ServerName_addServer_Form,__IPAddress_addServer_Form,__PortOpen_addServer_Form,__Hostname_addServer_Form,__ModSec_addServer_Form]
 		__cursor.callproc('WAF_Insert_ServerList',__args)
 		__conn.commit()
 		__Result = __cursor.fetchall()
+		print(__Result)
 		if __Result[0][0] == '0':
 			return render_template('addserver.html',errorMessage=__Result[0][1])
 		else:
@@ -119,17 +142,52 @@ def addServer():
 			if __ResultPOSTServer["Status"] == "0":
 				return render_template('addserver.html',errorMessage=__ResultPOSTServer["Message"])
 			else:
-				return redirect(url_for('server'))
+				flash("Server have been added Successfully!")
+				return redirect(url_for('home'))
 	else:
 		return render_template('addserver.html')
 
+@__app.route('/detail',methods=['POST'])
+def detailServer():
+	__ServerID_Detail_Page = request.form['ServerID']
+	__SecLogs_Detail_Return = []
+	__AccessLogs_Detail_Return = []
+	__ServerDetail_Detail_Return = None
+	GetLogsFromServer()
+
+	__args = [__ServerID_Detail_Page]
+	__cursor.callproc('WAF_Read_GetServerDetail',__args)
+	__conn.commit()
+	__Result = __cursor.fetchall()
+	print(__Result)
+	for x in __SecLogs_WAF_ReverseProxy:
+		if __Result[0][4] == x['transaction']['request']['headers']['Host']:
+			__SecLogs_Detail_Return.append(x)
+		
+	for x in __AccessLogs_Nginx_ReverseProxy:
+		if __Result[0][2] == x['server_pass']:
+			__AccessLogs_Detail_Return.append(x)
+
+	# print(json.dumps(__SecLogs_Detail_Return))
+	return jsonify({'SecurityLogs':json.dumps(__SecLogs_Detail_Return),'AccessLogs':json.dumps(__AccessLogs_Detail_Return)})
+	# return render_template('detail.html',SecurityLogs=__SecLogs_Detail_Return,AccessLogs=__AccessLogs_Detail_Return)
+
+@__app.route('/DetailLog',methods=['POST'])
+def detailLog():
+	GetLogsFromServer()
+	__SecLogs_Detail_Return = []
+
+	for x in __SecLogs_WAF_ReverseProxy:
+		if request.form['SecLogID'] == x['transaction']['id']:
+			__SecLogs_Detail_Return.append(x)
+
+	return json.dumps(__SecLogs_Detail_Return)
+	
 @__app.route('/login',methods=['POST'])
 def login():
 	_Username_Login_Form = request.form['username']
 	_Password_Login_Form = request.form['password']
-	# try:
 	error = None
-	
 	__args = [_Username_Login_Form,_Password_Login_Form]
 	__cursor.callproc('WAF_Read_Login',__args)
 	__conn.commit()
--- static/js/MainPageChart.js
+++ static/js/MainPageChart.js
@@ -1,124 +1,137 @@
 // 1
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
+	// The type of chart we want to create
+	type: 'line',
 
-    // The data for our dataset
-    data: {
-        labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-        datasets: [{
-            label: "Access Daily",
-            borderColor: '#97ce2f',
-            data: [0, 10, 5, 2, 20, 30, 45],
-        }]
-    },
+	// The data for our dataset
+	data: {
+		labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+		datasets: [{
+			label: "Access Daily",
+			borderColor: '#97ce2f',
+			data: [0, 10, 5, 2, 20, 30, 45],
+		}]
+	},
+	
 
-    // Configuration options go here
-    options: {}
+	// Configuration options go here
+	options: {
+		legend: {
+			display: false
+		},
+		title: {
+			display: true,
+			text: 'Custom Chart Title'
+		},
+	}
 });
 
 //2
 var ctx = document.getElementById('myChart2').getContext('2d');
 var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'bar',
+	// The type of chart we want to create
+	type: 'bar',
 
-    // The data for our dataset
-    data: {
-        labels: ["June", "July", "Augustus", "September"],
-        datasets: [{
-            label: "Total Attack",
-            backgroundColor: '#97ce2f',
-            borderColor: '#97ce2f',
-            data: [24, 26, 30, 40],
-        }]
-    },
+	// The data for our dataset
+	data: {
+		labels: ["June", "July", "Augustus", "September"],
+		datasets: [{
+			label: "Total Attack",
+			backgroundColor: '#97ce2f',
+			borderColor: '#97ce2f',
+			data: [24, 26, 30, 40],
+		}]
+	},
 
-    // Configuration options go here
-    options: {}
+	// Configuration options go here
+	options: {}
 });
 
 //3
 var ctx = document.getElementById('myChart3').getContext('2d');
 var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'horizontalBar',
+	// The type of chart we want to create
+	type: 'horizontalBar',
 
-    // The data for our dataset
-    data: {
-        labels: [ "GET", "POST", "PUT"],
-        datasets: [{
-            label: "Method",
-            backgroundColor: '#97ce2f',
-            borderColor: '#97ce2f',
-            data: [30, 10, 5],
-        }]
-    },
+	// The data for our dataset
+	data: {
+		labels: [ "GET", "POST", "PUT"],
+		datasets: [{
+			label: "Method",
+			backgroundColor: '#97ce2f',
+			borderColor: '#97ce2f',
+			data: [30, 10, 5],
+		}]
+	},
 
-    // Configuration options go here
-    options: {}
+	// Configuration options go here
+	options: {}
 });
 
 //4
 var ctx = document.getElementById('myChart4').getContext('2d');
 var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'doughnut',
+	// The type of chart we want to create
+	type: 'doughnut',
 
-    // The data for our dataset
-    data: {
-        labels: [ "200", "404", "502"],
-        datasets: [{
-            label: "Status Code",
-            backgroundColor: '#97ce2f',
-            borderColor: '#97ce2f',
-            data: [10, 10, 5],
-        }]
-    },
+	// The data for our dataset
+	data: {
+		labels: [ "200", "404", "502"],
+		datasets: [{
+			label: "Status Code",
+			backgroundColor: '#97ce2f',
+			borderColor: '#97ce2f',
+			data: [10, 10, 5],
+		}]
+	},
 
-    // Configuration options go here
-    options: {}
+	// Configuration options go here
+	options: {}
 });
 
 //5
 var ctx = document.getElementById('myChart5').getContext('2d');
 var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'radar',
+	// The type of chart we want to create
+	type: 'radar',
 
-    // The data for our dataset
-    data: {
-        labels: [ "Chrome", "FireFox", "Safari", "Komodo","IE"],
-        datasets: [{
-            label: "User Agent",
-            backgroundColor: '#97ce2f',
-            borderColor: '#97ce2f',
-            data: [14, 10, 5, 7, 8, 7],
-        }]
-    },
+	// The data for our dataset
+	data: {
+		labels: [ "Chrome", "FireFox", "Safari", "Komodo","IE"],
+		datasets: [{
+			label: "User Agent",
+			backgroundColor: '#97ce2f',
+			borderColor: '#97ce2f',
+			data: [14, 10, 5, 7, 8, 7],
+		}]
+	},
 
-    // Configuration options go here
-    options: {}
+	// Configuration options go here
+	options: {
+		legend: {
+			display: false
+		 },
+	}
 });
 
 //6
 var ctx = document.getElementById('myChart6').getContext('2d');
 var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'pie',
+	// The type of chart we want to create
+	type: 'pie',
 
-    // The data for our dataset
-    data: {
-        labels: [ "May", "June", "July"],
-        datasets: [{
-            label: "My First dataset",
-            backgroundColor: '#97ce2f',
-            borderColor: '#97ce2f',
-            data: [0, 10, 5, 2, 20, 30, 45],
-        }]
-    },
+	// The data for our dataset
+	data: {
+		labels: [ "May", "June", "July"],
+		datasets: [{
+			label: "My First dataset",
+			backgroundColor: '#97ce2f',
+			borderColor: '#97ce2f',
+			data: [0, 10, 5, 2, 20, 30, 45],
+		}]
+	},
 
-    // Configuration options go here
-    options: {}
+	// Configuration options go here
+	options: {}
 });
